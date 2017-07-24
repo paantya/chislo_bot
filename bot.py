ec986f9a1d09ad8974f9d05c9fe663d1e336d199
Patshin_Anton <paantya@gmail.com>
2017-07-24 18:44:11 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import time
+
 import telebot
 import telebot.types as types
 
+import re
 import cash
 import botan
 import config
@@ -40,24 +41,21 @@ def callback_inline(call):
 def callback_inline(call):
     if call.data == "test":
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
-        text = 'Добро пожаловать!\nВас приветствует бот СПбГУ!\nДля абитуариентов и студентов, у нас есть специальные разделы на сайте. \nУкажите пожалйуйста свой статус:'
+        text = 'Добро пожаловать!\nВыберите интересующий вас раздел:'
         keyboardStart = telebot.types.InlineKeyboardMarkup()
-        callback_button0 = telebot.types.InlineKeyboardButton(text="Абитуриент", callback_data="abitur")
-        callback_button1 = telebot.types.InlineKeyboardButton(text="Студент", callback_data="students")
+        callback_button0 = telebot.types.InlineKeyboardButton(text="Клиент", callback_data="cl")
+        callback_button1 = telebot.types.InlineKeyboardButton(text="Гость", callback_data="g")
         keyboardStart.add(callback_button0,callback_button1)
         bot.send_message(call.message.chat.id, text,reply_markup=keyboardStart)
-    if call.data == "event":
-        text = "Вы выбрали раздел:\nМероприятия"
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
-        bot.send_message(call.message.chat.id, text)
-    if call.data == "abitur":
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        for text in cash.keyb["abitur"]:
-            callback_button = telebot.types.InlineKeyboardButton(text=text, callback_data=cash.keyb["abitur"][text])
-            keyboard.add(callback_button)
-        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
-        bot.send_message(call.message.chat.id, text='Вы Выбрали раздел Абитуриентов.', reply_markup=keyboard)
-    if call.data == "students":
+    if call.data == "cl":
+        text = "Для записи на консультацию *позвоните* по номеру:\n\n[+79165127377](tel:+79165127377)"
+        # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
+        bot.send_message(call.message.chat.id, text, parse_mode='Markdown')
+    if call.data == "g":
+        text = "Для посчёта вашего числа введите полностью *ФИО* или *дату рождения*.\nДата рождения в формате:\n*ГГГГ ММ ДД* или *ДД ММ ГГГГ*"
+        # bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=call.message.text)
+        bot.send_message(call.message.chat.id, text, parse_mode='Markdown')
+    if call.data == "sll":
         keyboard = telebot.types.InlineKeyboardMarkup()
         for text in cash.keyb["students"]:
             callback_button = telebot.types.InlineKeyboardButton(text=text, callback_data=cash.keyb["students"][text])
@@ -68,23 +66,24 @@ def callback_inline(call):
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     botan.track(config.botan_key, message.chat.id, message, '/start')
-    text = 'Добро пожаловать!\nВас приветствует бот СПбГУ!\nДля абитуариентов и студентов, у нас есть специальные разделы на сайте. \nУкажите пожалйуйста свой статус:'
+    text = 'Добро пожаловать!\nЯ помогу тебе разобраться с числами!\nВыберите интересующий вас раздел:'
     keyboardStart = telebot.types.InlineKeyboardMarkup()
-    callback_button0 = telebot.types.InlineKeyboardButton(text="Абитуриент", callback_data="abitur")
-    callback_button1 = telebot.types.InlineKeyboardButton(text="Студент", callback_data="students")
-    keyboardStart.add(callback_button0,callback_button1)
-    bot.send_message(message.chat.id, text,reply_markup=keyboardStart)
+    callback_button0 = telebot.types.InlineKeyboardButton(text="Посчитать моё число", callback_data="g")
+    callback_button1 = telebot.types.InlineKeyboardButton(text="Записаться на консультацию", callback_data="cl")
+    keyboardStart.add(callback_button0)
+    keyboardStart.add(callback_button1)
+    bot.send_message(message.chat.id, text, reply_markup=keyboardStart)
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
     botan.track(config.botan_key, message.chat.id, message, '/help')
-    text = "Помошь:\n/help - это сообещние\n/about - о боте\n\nДля содсчёт собственного числа имени введите ФИО полностью.\nДля подсчёта собственного числа даты вождения введите дату рождения в формате ДД ММ ГГГГ или ГГГГ ММ ДД."
+    text = 'Помошь:\n/help - это сообещние\n/about - о боте\n\nДля содсчёт "собственного числа имени" введите ФИО полностью.\nДля подсчёта "собственного числа даты вождения" введите дату рождения в формате ДД ММ ГГГГ или ГГГГ ММ ДД.'
     bot.send_message(message.chat.id, text)
 
 @bot.message_handler(commands=['about'])
 def send_about(message):
     botan.track(config.botan_key, message.chat.id, message, '/about')
-    text = 'Бот предназначен для помощи в посчёте своих чисел.\nОбратная связь: @pa_antya'
+    text = 'Бот предназначен для помощи в посчёте "своих чисел".\nОбратная связь/разработчик: @pa_antya'
     bot.send_message(message.chat.id, text)
 
 @bot.message_handler(commands=['test'])
@@ -99,17 +98,39 @@ def send_help(message):
 # Обычный режим
 @bot.message_handler(content_types=["text"])
 def any_msg(message):
-    botan.track(config.botan_key, message.chat.id, message, 'empty_text')
-    keyboard = telebot.types.InlineKeyboardMarkup()
-    callback_button = telebot.types.InlineKeyboardButton(text="Нажми меня", callback_data="test")
-    keyboard.add(callback_button)
-    bot.send_message(message.chat.id, "Что-то не то ты ввел.", reply_markup=keyboard)
+    print(message.text)
+    pdate = re.compile(r'(\d{1,4})[\-_:;\\\|\/ ,.]{0,}(\d{1,2})[\-_:;\\\|\/ ,.]{0,}(\d{1,4})')
+    pfio = re.compile(r'([a-zA-ZА-Яа-яёЁ\-\s]{2,})')
 
+    mfio = pfio.match(message.text)
+    mdate = pdate.match(message.text)
+
+    print(mfio)
+    print(mdate)
+    if (mfio or mdate):
+        if mdate:
+            print('Match found: "{}"'.format(mdate.group()))
+            chislo = chisla_fun.date(message)
+        if mfio:
+            print('Match found: "{}"'.format(mfio.group()))
+            chislo = chisla_fun.fio(message)
+        text = 'Вы ввели: *{}*\nВыше число: *{}*'.format(message.text, chislo)
+        bot.send_message(message.chat.id, text, parse_mode='Markdown')
+    else:
+        text = 'Введены *не верные* данные.\nПерепроверьте, пожалуйста:\n{}\n*Если* данные *верные* - свяжитесь с разработчиком в раздете /about.'.format(message.text)
+        bot.send_message(message.chat.id, text,parse_mode='Markdown')
+
+    text = 'Выберите интересующий вас раздел:'
+'''keyboardText = telebot.types.InlineKeyboardMarkup()
+    callback_button0 = telebot.types.InlineKeyboardButton(text="Клиент", callback_data="cl")
+    callback_button1 = telebot.types.InlineKeyboardButton(text="Гость", callback_data="g")
+    keyboardText.add(callback_button0, callback_button1)
+    bot.send_message(message.chat.id, text, reply_markup=keyboardText)
+'''
 if __name__ == '__main__':
-    try:
-        bot.polling(none_stop=True)
-    except: Exception
-        print('error Exception')
-    
+    #try:
+    bot.polling(none_stop=True)
+    #except Exception:
+    #    print('error Exception')
 
 #YandexMetrica.setCustomAppVersion("1.13.2");
